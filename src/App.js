882165f21d686704bc0182b3c8f544ee47e1292a
anateresavasques <anatcvasques@hotmail.com>
2022-02-23 08:21:50 +0000

    added the github repo url

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a href="https://portfolio-atcv.netlify.app/" target="_blank">
+          Ana Teresa
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/anateresavasques/reactweatherapp"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
