a3fb9155b33826ddd53e48f827947c6b71f92d11
anateresavasques <anatcvasques@hotmail.com>
2022-02-23 16:15:16 +0000

    js e css

--- src/App.js
+++ src/App.js
@@ -8,13 +8,14 @@ export default function App() {
       <Weather />
       <footer>
         This project was coded by{" "}
-        <a href="https://portfolio-atcv.netlify.app/" target="_blank">
+        <a href="https://portfolio-atcv.netlify.app/" target="_blank" id="link">
           Ana Teresa
         </a>{" "}
         and is{" "}
         <a
           href="https://github.com/anateresavasques/reactweatherapp"
           target="_blank"
+          id="link"
         >
           open-sourced on GitHub
         </a>
--- src/Weather.js
+++ src/Weather.js
@@ -3,40 +3,54 @@ import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div className="Weather">
-      <form>
+    <div className="weather-app-wrapper">
+      <div className="weather-app">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Type a city..."
+                className="form-control"
+              />{" "}
+            </div>
+
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                class="btn btn-primary w-100"
+                id="searchButton"
+              />
+              <button
+                className="btn btn-success w-100"
+                id="current-location-button"
+              >
+                &#128204; Me
+              </button>
+            </div>
+          </div>
+        </form>
+        <h1>New York</h1>
+        <ul>
+          <li>Wednesday 07:00 </li>
+          <li>Mostly Cloudy</li>
+        </ul>
         <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Type a city..."
-              className="form-control"
-            />{" "}
+          <div className="col-6">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Mostly Cloudy"
+            />
+            6ºC
           </div>
-          <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+          <div className="col-6">
+            <ul>
+              <li>Precipitation:15%</li>
+              <li>Humidity: 72%</li>
+              <li>Wind:13 kn/h</li>
+            </ul>
           </div>
-        </div>
-      </form>
-      <h1>New York</h1>
-      <ul>
-        <li>Wednesday 07:00 </li>
-        <li>Mostly Cloudy</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Mostly Cloudy"
-          />
-          6ºC
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation:15%</li>
-            <li>Humidity: 72%</li>
-            <li>Wind:13 kn/h</li>
-          </ul>
         </div>
       </div>
     </div>
